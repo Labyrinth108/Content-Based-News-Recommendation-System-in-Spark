8fb54bb11586e4d71a4ee2316c56f7981399ac19
luoyi <luoyihh@gmail.com>
2017-02-07 23:17:05 +0800

    filter the stopword according to the speech

--- code/Python/process_data.py
+++ code/Python/process_data.py
@@ -8,18 +8,18 @@ def changeToTimestamp(sep_time):
     timestamp = int(time.mktime(time_struct))
     return timestamp
 
-def get_data(filename, sep, names, septime = "2014-03-20 23:59:00"):
+def get_data(filename, sep, names):
     raw_data = pd.read_table(filename, sep='\t', header=None, names=names).dropna(how='any')
 
     read_times = raw_data['read_time']
     sep_time = "2014-03-20 23:59:00" # the time dividing train data and test data
     timestamp = changeToTimestamp(sep_time)
 
-    index_train_data = read_times.index[read_times < timestamp]
-    index_test_data = read_times.index[read_times >= timestamp]
+    before_sep_data = read_times.index[read_times < timestamp]
+    after_sep_data = read_times.index[read_times >= timestamp]
 
-    training_data = raw_data.drop(index_train_data)
-    testing_data = raw_data.drop(index_test_data)
+    training_data = raw_data.drop(after_sep_data)
+    testing_data = raw_data.drop(before_sep_data)
 
     return raw_data, training_data, testing_data
 
@@ -28,16 +28,20 @@ if __name__ == '__main__':
     sep = '\t'
     names = ['user_id', 'news_id', 'read_time', 'news_title', 'news_content', 'news_publi_time']
     raw_data, training_data, testing_data = get_data(filename, sep, names=names)
+
+    # calculateNumber(training_data)
+    # calculateNumber(testing_data)
+
     training_data_news = training_data.drop_duplicates(['news_title'])
+
+    # calculateNumber(training_data_news)
+
     # test_user_id = set(list(testing_data['user_id'].values))
     # tran_user_id = set(list(training_data['user_id'].values))
     #
     # new_user_test = test_user_id - tran_user_id
     # old_user_test = test_user_id - new_user_test
-    # np.save('/Users/luoyi/Documents/Python/RecommendationSystem/data/test_user_all_id.npy', test_user_id)
-    # np.save('/Users/luoyi/Documents/Python/RecommendationSystem/data/test_old_user_id.npy', old_user_test)
-    # np.save('/Users/luoyi/Documents/Python/RecommendationSystem/data/test_new_user_id.npy', new_user_test)
 
-    news_train = [preprocess(training_data_news['news_title'].values[i] + "_"+ training_data_news['news_content'].values[i])
-                  for i in range(0,500)]
+    news_train = [preprocess_WithSpeech(training_data_news['news_title'].values[i] + "_"+ training_data_news['news_content'].values[i])
+                  for i in range(0,4284)]
     writeInFile(news_train, '/Users/luoyi/Documents/Python/RecommendationSystem/data/news.txt')
--- code/Python/utils.py
+++ code/Python/utils.py
@@ -1,18 +1,55 @@
 # -*- coding: UTF-8 -*-
 import jieba
-import io
-import jieba.analyse
+import io, re
+import jieba.analyse, jieba.posseg
+
+def calculateNumber(df):
+    print(len(df.index))
 
 def writeInFile(content, filename):
-    file = io.open(filename, 'w', encoding='utf-8')
+    file = io.open(filename, 'w', encoding="utf-8")
     for news in content:
-        file.write(news + "\n")
+        try:
+            file.write(news + "\n")
+        except Exception as e:
+            print(news)
     file.close()
+
+def hasNumbersAlphas(inputString):
+    if re.search("\w", inputString):
+        return True
+    return False
+
+def hasChineseNumbers(intputString):
+    ChineseNumbers = ['一','二','三','四','五','六','七','八','九','十']
+    return any(number in intputString for number in ChineseNumbers)
 
 def preprocess(news):
     content = jieba.cut(news, cut_all=False)  # 精确模式分词
     content = filter(lambda x: len(x) > 1, content)  # 过滤掉长度为1 的词（排除干扰）
-    content = filter(lambda x: not x.isdigit(), content)  # 过滤掉长度为1 的词（排除干扰）
+    content = filter(lambda x: not hasChineseNumbers(x.encode("utf-8")), content)
+    content = filter(lambda x: not hasNumbersAlphas(x), content)  # 过滤掉含有数字和字母的词
+
+    stopw = [line.strip().decode('utf-8') for line in open('/Users/luoyi/Documents/Python/RecommendationSystem/data/stopwords.txt').readlines()]
+
+    parsed = set(content) - set(stopw)
+    return ' '.join(parsed)
+
+def hasMeaningfulWords(x):
+    flag = x.flag
+    target_set = ['n', 'ns', 'ni','nh','nt', 'nz','a','ad','an','d','v','vd','vn','vi','vl']
+
+    if flag in target_set:
+        return True
+    return False
+
+def preprocess_WithSpeech(news):
+    content = jieba.posseg.cut(news)  # 精确模式分词
+
+    content = filter(lambda x: hasMeaningfulWords(x), content)  # 留下名词，形容词，副词
+    content = [i.word for i in content]
+    content = filter(lambda x: len(x) > 1, content)  # 过滤掉单字
+
     stopw = [line.strip().decode('utf-8') for line in open('/Users/luoyi/Documents/Python/RecommendationSystem/data/stopwords.txt').readlines()]
 
     parsed = set(content) - set(stopw)
